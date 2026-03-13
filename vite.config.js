943a97a668112eceedebbfeb0548a6b73fb66357
locvo-app <v.thanh.loc@gmail.com>
2026-03-13 19:23:43 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Cấu hình công cụ đóng gói Vite cho React
-export default defineConfig({
-  plugins: [react()],
-})
-
